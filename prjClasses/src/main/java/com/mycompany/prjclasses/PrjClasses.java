5be2ced6c06bb712a4f1e43e7673fa57e980402a
M1mzie <ST10470762@vcconnect.edu.za>
2025-03-04 11:38:56 +0200

    Updated the Project
    Added a new line of code, following Reece's instructions

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -12,5 +12,6 @@ public class PrjClasses {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("I just did my first GitHub activity!!");
     }
 }
