a1758a2bf5f384cd6958b86a2cb70072deb0262c
M1mzie <ST10470762@vcconnect.edu.za>
2025-03-07 11:50:19 +0200

    Added to Activity with Lecturer
    Added a class to test how to use a class

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -13,5 +13,11 @@ public class PrjClasses {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I just did my first GitHub activity!!");
+        
+        newClass nc = new newClass(); // creating object of newClass
+        
+        nc.hello(); // have to use variable name nc to communicate with the class
+        
+        System.out.println(nc.strSecondClassVar); // output variable from 2nd class
     }
 }
